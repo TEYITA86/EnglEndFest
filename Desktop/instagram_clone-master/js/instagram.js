dc726b7825786c68d58257aedfb93077645567a4
TEYITA86 <68133555+TEYITA86@users.noreply.github.com>
2020-10-30 12:02:46 -0300

    cambio prueba

---
+++ Desktop/instagram_clone-master/js/instagram.js
@@ -0,0 +1,52 @@
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+const swiperWrapper = document.querySelector("#swiperWrapper");
+
+fetch("./api/historias.json")
+.then((rest)=>rest.json())
+.then((rest)=>historias(rest))
+
+
+
+
+function historiaItem(his){
+  return`
+ <div>
+  <img src="${his.foto}" /> 
+  <p>${his.usuario}</p> 
+ </div>
+`
+}
+
+function historias(rest) {
+  const valor = rest.reduce((acumulador, item)=>{
+   return acumulador + historiaItem(item)
+  },"")
+ swiperWrapper.innerHTML=valor 
+
+} 
+
+
+// TITULO DE INSTAGRAM
+
+document.getElementById('title').innerHTML = 'Instagram';
+document.getElementById('buscar').placeholder='Buscar';
+
+
+
